d33df033cfb32f5c9f9a33971cc3c343b9df0633
robertoBosio <roberto.bosio98@gmail.com>
2025-12-02 15:22:07 +0000

    feat: Added ReLU operator

---
+++ nn2fpga/library/include/StreamingReLU.hpp
@@ -0,0 +1,156 @@
+#pragma once
+#include "ap_int.h"
+#include "hls_math.h"
+#include <type_traits>
+#include "hls_stream.h"
+#include "utils/CSDFG_utils.hpp"
+#include <cassert>
+#include <cstddef>
+#include <unordered_map>
+
+template <typename T> struct ReLU {
+  T operator()(T acc) const {
+#pragma HLS inline
+    if (acc < 0)
+      return T(0);
+    else
+      return acc;
+  }
+};
+
+template <typename TInputWord, typename TInput, typename TOutputWord,
+          typename TOutput, size_t IN_HEIGHT, size_t IN_WIDTH, size_t IN_CH,
+          size_t CH_PAR, size_t W_PAR>
+class StreamingReLU {
+public:
+  static_assert(IN_CH % CH_PAR == 0, "IN_CH must be a multiple of CH_PAR");
+  static_assert(CH_PAR > 0, "CH_PAR must be greater than 0");
+  static_assert(IN_HEIGHT > 0 && IN_WIDTH > 0,
+                "IN_HEIGHT and IN_WIDTH must be greater than 0");
+
+  StreamingReLU() = default;
+
+  struct StepState {
+    // Loop iteration indexes.
+    size_t i_hw = 0, i_ch = 0;
+
+    PipelineDelayBuffer<TOutputWord> delayed_output[W_PAR];
+    ActorStatus actor_status{1, 1};
+    bool initialized = false;
+
+    void init(size_t depth) {
+      if (initialized)
+        return;
+      for (size_t i = 0; i < W_PAR; i++) {
+        delayed_output[i] = PipelineDelayBuffer<TOutputWord>(depth);
+      }
+      actor_status =
+          ActorStatus(depth, IN_HEIGHT * IN_WIDTH * IN_CH / (CH_PAR * W_PAR));
+      initialized = true;
+    }
+  };
+
+  using Registry = std::unordered_map<const void *, StepState>;
+  static Registry &registry() {
+    static Registry r;
+    return r;
+  }
+
+  void step_init(size_t pipeline_depth = 1) {
+    auto &st = registry()[this];
+    st.init(pipeline_depth);
+  }
+
+  template <size_t HLS_TAG>
+  void run(hls::stream<TInputWord> i_data[W_PAR], hls::stream<TOutputWord> o_data[W_PAR]) {
+    // Loop through the input height and width.
+    for (size_t i_hw = 0; i_hw < IN_HEIGHT * IN_WIDTH / W_PAR; i_hw++) {
+    STREAMINGRELU_RUN_LOOP:
+      for (size_t i_ch = 0; i_ch < IN_CH / CH_PAR; i_ch++) {
+#pragma HLS pipeline II = 1
+        StreamingReLU::pipeline_body(i_data, o_data);
+      }
+    }
+  }
+
+  ActorStatus step(hls::stream<TInputWord> i_data[W_PAR],
+                   hls::stream<TOutputWord> o_data[W_PAR]) {
+    // Get the state for this instance.
+    auto it = registry().find(this);
+    assert(it != registry().end() && "Instance not initialized");
+    auto &st = it->second;
+
+    // Compute firing condition.
+    bool firing_condition = true;
+
+    // Check non empty input streams.
+    for (size_t i_in_stream = 0; i_in_stream < W_PAR; i_in_stream++) {
+      if (i_data[i_in_stream].empty()) {
+        firing_condition = false;
+      }
+    }
+
+    if (firing_condition) {
+
+      hls::stream<TOutputWord> instant_output_stream[W_PAR];
+      StreamingReLU::pipeline_body(i_data, instant_output_stream);
+
+      st.actor_status.fire();
+
+      // Add the output to the delayed output stream.
+      for (size_t w_par = 0; w_par < W_PAR; w_par++) {
+        st.delayed_output[w_par].push(instant_output_stream[w_par].read(),
+                                      true);
+      }
+
+      // Update the counters.
+      st.i_ch++;
+      if (st.i_ch >= IN_CH / CH_PAR) {
+        // If we have processed all output channels, reset the index and
+        // increment the height/width index.
+        st.i_ch = 0;
+        st.i_hw++;
+      }
+      if (st.i_hw >= IN_HEIGHT * IN_WIDTH / W_PAR) {
+        st.i_hw = 0; // Reset the height/width index if we have processed all
+                     // iterations.
+      }
+
+    } else {
+      // If there is no data in the input stream, push a delay slot.
+      for (size_t i_w_par = 0; i_w_par < W_PAR; ++i_w_par) {
+        st.delayed_output[i_w_par].push(TOutputWord(), false);
+      }
+    }
+
+    // Advance the state of the actor firings.
+    st.actor_status.advance();
+
+    // Write the output data to the output stream.
+    TOutputWord out;
+    for (size_t i_w_par = 0; i_w_par < W_PAR; i_w_par++) {
+      if (st.delayed_output[i_w_par].pop(out)) {
+        o_data[i_w_par].write(out);
+      }
+    }
+
+    // Return the current actor status.
+    return st.actor_status;
+  }
+
+private:
+  static void pipeline_body(hls::stream<TInputWord> i_data[W_PAR],
+                            hls::stream<TOutputWord> o_data[W_PAR]) {
+#pragma HLS inline
+    ReLU<TInput> relu;
+    for (size_t w_par = 0; w_par < W_PAR; w_par++) {
+      TInputWord in_word = i_data[w_par].read();
+      TOutputWord out_word;
+      for (size_t ch_par = 0; ch_par < CH_PAR; ch_par++) {
+        TOutput out_value = relu(in_word[ch_par]);
+        out_word[ch_par] = out_value;
+      }
+      o_data[w_par].write(out_word);
+    }
+  }
+};
---
+++ tests/test_StreamingReLU.py
@@ -0,0 +1,140 @@
+import numpy as np
+import onnxruntime as ort
+import csnake
+from onnx import TensorProto, helper
+from .base_hls_test import BaseHLSTest
+
+class TestStreamingReLU(BaseHLSTest):
+
+    @property
+    def operator_filename(self):
+        return "StreamingReLU"
+
+    @property
+    def unit_filename(self):
+        return "StreamingReLU"
+
+    def generate_config_file(self, config_dict):
+
+        # random tensors
+        input_tensor = np.random.randint(
+            -128,
+            127,
+            size=(
+                1,
+                config_dict["IN_CH"],
+                config_dict["IN_HEIGHT"],
+                config_dict["IN_WIDTH"],
+            ),
+            dtype=np.int8,
+        )
+
+        X = helper.make_tensor_value_info(
+            "X",
+            TensorProto.INT8,
+            [
+                1,
+                config_dict["IN_CH"],
+                config_dict["IN_HEIGHT"],
+                config_dict["IN_WIDTH"],
+            ],
+        )
+        Y = helper.make_tensor_value_info(
+            "Y",
+            TensorProto.INT8,
+            [
+                1,
+                config_dict["IN_CH"],
+                config_dict["IN_HEIGHT"],
+                config_dict["IN_WIDTH"],
+            ],
+        )
+
+        X_scale = helper.make_tensor(
+            "X_scale", TensorProto.FLOAT, [], [config_dict["X_SCALE"]]
+        )
+        X_zp = helper.make_tensor("X_zp", TensorProto.INT8, [], [config_dict["X_ZP"]])
+        Y_scale = helper.make_tensor(
+            "Y_scale", TensorProto.FLOAT, [], [config_dict["Y_SCALE"]]
+        )
+        Y_zp = helper.make_tensor("Y_zp", TensorProto.INT8, [], [config_dict["Y_ZP"]])
+
+        dqlinear = helper.make_node(
+            "DequantizeLinear",
+            inputs=["X", "X_scale", "X_zp"],
+            outputs=["X_dq"],
+        )
+
+        global_avg_pool = helper.make_node(
+            "Relu",
+            inputs=["X_dq"],
+            outputs=["Y_dq"],
+        )
+
+        qlinear = helper.make_node(
+            "QuantizeLinear",
+            inputs=["Y_dq", "Y_scale", "Y_zp"],
+            outputs=["Y"],
+        )
+
+        graph = helper.make_graph(
+            [dqlinear, global_avg_pool, qlinear],
+            "qconv_test",
+            [X],
+            [Y],
+            initializer=[X_scale, X_zp, Y_scale, Y_zp],
+        )
+        model = helper.make_model(graph, producer_name="qonnx")
+        sess = ort.InferenceSession(
+            model.SerializeToString(), providers=["CPUExecutionProvider"]
+        )
+        y = sess.run(None, {"X": input_tensor})[0]
+
+        cwr = csnake.CodeWriter()
+        cwr.include("<cstdint>")
+        cwr.include("<array>")
+        cwr.include("<ap_int.h>")
+        cwr.add_line("namespace test_config {")
+        cwr.indent()
+        for key, value in config_dict.items():
+            if key in ["X_SCALE", "W_SCALE", "Y_SCALE"]:
+                cwr.add_line(f"const float {key} = {value}f;")
+            else:
+                cwr.add_line(f"const int {key} = {value};")
+        cwr.add_line(f"typedef ap_int<{config_dict['INPUT_DATAWIDTH']}> TInput;")
+        cwr.add_line(f"typedef ap_uint<{config_dict['OUTPUT_DATAWIDTH']}> TOutput;")
+        cwr.add_lines(
+            csnake.Variable(
+                "input_tensor",
+                primitive=f"ap_int<{config_dict['INPUT_DATAWIDTH']}>",
+                value=input_tensor,
+            ).generate_initialization()
+        )
+        cwr.add_lines(
+            csnake.Variable(
+                "output_tensor",
+                primitive=f"ap_int<{config_dict['OUTPUT_DATAWIDTH']}>",
+                value=y,
+            ).generate_initialization()
+        )
+        cwr.dedent()
+        cwr.add_line("}")
+        return cwr.code
+
+    def test_7x7_po2(self, hls_steps):
+        np.random.seed(42)
+        config_dict = {
+            "INPUT_DATAWIDTH": 8,
+            "OUTPUT_DATAWIDTH": 8,
+            "IN_HEIGHT": 2,
+            "IN_WIDTH": 2,
+            "IN_CH": 2,
+            "CH_PAR": 2,
+            "W_PAR": 2,
+            "X_SCALE": 2**-5,
+            "Y_SCALE": 2**-5,
+            "X_ZP": 0,
+            "Y_ZP": 0,
+            "PIPELINE_DEPTH": 5,
+        }
+        self.run(config_dict, hls_steps)
