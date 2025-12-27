b604da11c0c9d15aecd6321f9da1f8e44987b977
robertoBosio <roberto.bosio98@gmail.com>
2025-12-27 14:40:29 +0000

    test: Added unsigned case in conv

--- tests/test_StreamingConv.py
+++ tests/test_StreamingConv.py
@@ -9,10 +9,30 @@ class TestStreamingConv(BaseHLSTest):
     @property
     def operator_filename(self) -> str:
         return "StreamingConv"
-    
+
     @property
     def unit_filename(self) -> str:
         return "StreamingConv"
+
+    def get_tensorproto_dtype(self, datawidth, is_unsigned):
+        if datawidth == 8:
+            return TensorProto.UINT8 if is_unsigned else TensorProto.INT8
+        elif datawidth == 16:
+            return TensorProto.UINT16 if is_unsigned else TensorProto.INT16
+        elif datawidth == 32:
+            return TensorProto.UINT32 if is_unsigned else TensorProto.INT32
+        else:
+            raise ValueError(f"Unsupported datawidth: {datawidth}")
+
+    def get_numpy_dtype(self, datawidth, is_unsigned):
+        if datawidth == 8:
+            return np.uint8 if is_unsigned else np.int8
+        elif datawidth == 16:
+            return np.uint16 if is_unsigned else np.int16
+        elif datawidth == 32:
+            return np.uint32 if is_unsigned else np.int32
+        else:
+            raise ValueError(f"Unsupported datawidth: {datawidth}")
 
     def generate_config_file(self, config_dict):
         IN_HEIGHT = (
@@ -32,33 +52,53 @@ class TestStreamingConv(BaseHLSTest):
 
         # random tensors
         input_tensor = np.random.randint(
-            -128,
-            127,
+            0 if config_dict.get("INPUT_IS_UNSIGNED", False) else -128,
+            255 if config_dict.get("INPUT_IS_UNSIGNED", False) else 127,
             size=(1, config_dict["IN_CH"], IN_HEIGHT, IN_WIDTH),
-            dtype=np.int8,
+            dtype=self.get_numpy_dtype(
+                config_dict["INPUT_DATAWIDTH"],
+                config_dict.get("INPUT_IS_UNSIGNED", False),
+            ),
         )
         weight_tensor = np.random.randint(
-            -128,
-            127,
+            0 if config_dict.get("WEIGHT_IS_UNSIGNED", False) else -128,
+            255 if config_dict.get("WEIGHT_IS_UNSIGNED", False) else 127,
             size=(
                 config_dict["OUT_CH"],
                 config_dict["IN_CH"],
                 config_dict["FH"],
                 config_dict["FW"],
             ),
-            dtype=np.int8,
+            dtype=self.get_numpy_dtype(
+                config_dict["WEIGHT_DATAWIDTH"],
+                config_dict.get("WEIGHT_IS_UNSIGNED", False),
+            ),
         )
         bias_tensor = np.random.randint(
-            -1000, 1000, size=(config_dict["OUT_CH"],), dtype=np.int32
+            0 if config_dict.get("BIAS_IS_UNSIGNED", False) else -10000,
+            20000 if not config_dict.get("BIAS_IS_UNSIGNED", False) else 10000,
+            size=(config_dict["OUT_CH"],),
+            dtype=self.get_numpy_dtype(
+                config_dict["BIAS_DATAWIDTH"],
+                config_dict.get("BIAS_IS_UNSIGNED", False),
+            ),
         )
 
         # IO
         X = helper.make_tensor_value_info(
-            "X", TensorProto.INT8, [1, config_dict["IN_CH"], IN_HEIGHT, IN_WIDTH]
+            "X",
+            self.get_tensorproto_dtype(
+                config_dict.get("INPUT_DATAWIDTH", 8),
+                config_dict.get("INPUT_IS_UNSIGNED", False),
+            ),
+            [1, config_dict["IN_CH"], IN_HEIGHT, IN_WIDTH],
         )
         Y = helper.make_tensor_value_info(
             "Y",
-            TensorProto.INT8,
+            self.get_tensorproto_dtype(
+                config_dict.get("OUTPUT_DATAWIDTH", 8),
+                config_dict.get("OUTPUT_IS_UNSIGNED", False),
+            ),
             [
                 1,
                 config_dict["OUT_CH"],
@@ -70,7 +110,10 @@ class TestStreamingConv(BaseHLSTest):
         # Inits
         W = helper.make_tensor(
             "W",
-            TensorProto.INT8,
+            self.get_tensorproto_dtype(
+                config_dict.get("WEIGHT_DATAWIDTH", 8),
+                config_dict.get("WEIGHT_IS_UNSIGNED", False),
+            ),
             [
                 config_dict["OUT_CH"],
                 config_dict["IN_CH"],
@@ -79,24 +122,52 @@ class TestStreamingConv(BaseHLSTest):
             ],
             weight_tensor.flatten().tolist(),
         )
+
         B = helper.make_tensor(
             "B",
-            TensorProto.INT32,
+            self.get_tensorproto_dtype(
+                config_dict.get("BIAS_DATAWIDTH", 32),
+                config_dict.get("BIAS_IS_UNSIGNED", False),
+            ),
             [config_dict["OUT_CH"]],
             bias_tensor.flatten().tolist(),
         )
         X_scale = helper.make_tensor(
             "X_scale", TensorProto.FLOAT, [], [config_dict["X_SCALE"]]
         )
-        X_zp = helper.make_tensor("X_zp", TensorProto.INT8, [], [config_dict["X_ZP"]])
+        X_zp = helper.make_tensor(
+            "X_zp",
+            self.get_tensorproto_dtype(
+                config_dict.get("INPUT_DATAWIDTH", 8),
+                config_dict.get("INPUT_IS_UNSIGNED", False),
+            ),
+            [],
+            [config_dict["X_ZP"]],
+        )
         W_scale = helper.make_tensor(
             "W_scale", TensorProto.FLOAT, [], [config_dict["W_SCALE"]]
         )
-        W_zp = helper.make_tensor("W_zp", TensorProto.INT8, [], [config_dict["W_ZP"]])
+        W_zp = helper.make_tensor(
+            "W_zp",
+            self.get_tensorproto_dtype(
+                config_dict.get("WEIGHT_DATAWIDTH", 8),
+                config_dict.get("WEIGHT_IS_UNSIGNED", False),
+            ),
+            [],
+            [config_dict["W_ZP"]],
+        )
         Y_scale = helper.make_tensor(
             "Y_scale", TensorProto.FLOAT, [], [config_dict["Y_SCALE"]]
         )
-        Y_zp = helper.make_tensor("Y_zp", TensorProto.INT8, [], [config_dict["Y_ZP"]])
+        Y_zp = helper.make_tensor(
+            "Y_zp",
+            self.get_tensorproto_dtype(
+                config_dict.get("OUTPUT_DATAWIDTH", 8),
+                config_dict.get("OUTPUT_IS_UNSIGNED", False),
+            ),
+            [],
+            [config_dict["Y_ZP"]],
+        )
 
         qconv = helper.make_node(
             "QLinearConv",
@@ -149,40 +220,57 @@ class TestStreamingConv(BaseHLSTest):
             if key in ["X_SCALE", "W_SCALE", "Y_SCALE"]:
                 cwr.add_line(f"const float {key} = {value}f;")
             else:
-                cwr.add_line(f"const int {key} = {value};")
-        cwr.add_line(f"typedef ap_int<{config_dict['INPUT_DATAWIDTH']}> TInput;")
-        cwr.add_line(f"typedef ap_int<{config_dict['WEIGHT_DATAWIDTH']}> TWeight;")
-        cwr.add_line(f"typedef ap_int<{config_dict['BIAS_DATAWIDTH']}> TBias;")
-        cwr.add_line(f"typedef ap_int<{config_dict['OUTPUT_DATAWIDTH']}> TOutput;")
-        cwr.add_line(f"typedef ap_int<{config_dict['ACC_DATAWIDTH']}> TAcc;")
-        cwr.add_line(f"typedef ap_int<{config_dict['ACC_DATAWIDTH']}> TPartialAcc;")
+                if isinstance(value, bool):
+                    value_str = "true" if value else "false"
+                    cwr.add_line(f"const bool {key} = {value_str};")
+                else:
+                    cwr.add_line(f"const int {key} = {value};")
+        typedef_suffix = "u" if config_dict.get("INPUT_IS_UNSIGNED", False) else ""
+        cwr.add_line(f"typedef ap_{typedef_suffix}int<{config_dict['INPUT_DATAWIDTH']}> TInput;")
+        typedef_suffix = "u" if config_dict.get("WEIGHT_IS_UNSIGNED", False) else ""
+        cwr.add_line(f"typedef ap_{typedef_suffix}int<{config_dict['WEIGHT_DATAWIDTH']}> TWeight;")
+        typedef_suffix = "u" if config_dict.get("BIAS_IS_UNSIGNED", False) else ""
+        cwr.add_line(f"typedef ap_{typedef_suffix}int<{config_dict['BIAS_DATAWIDTH']}> TBias;")
+        typedef_suffix = "u" if config_dict.get("OUTPUT_IS_UNSIGNED", False) else ""
+        cwr.add_line(f"typedef ap_{typedef_suffix}int<{config_dict['OUTPUT_DATAWIDTH']}> TOutput;")
+        typedef_suffix = (
+            "u"
+            if (
+                config_dict.get("INPUT_IS_UNSIGNED", False)
+                and config_dict.get("WEIGHT_IS_UNSIGNED", False)
+                and config_dict.get("BIAS_IS_UNSIGNED", False)
+            )
+            else ""
+        )
+        cwr.add_line(f"typedef ap_{typedef_suffix}int<{config_dict['ACC_DATAWIDTH']}> TAcc;")
+        cwr.add_line(f"typedef ap_{typedef_suffix}int<{config_dict['ACC_DATAWIDTH'] - 1}> TPartialAcc;")
         cwr.add_line(f"typedef DequantQuantPo2<{shift}, TAcc, TOutput> Quantizer;")
         cwr.add_line(f"typedef DequantQuantEqual<TAcc> Activation;")
         cwr.add_lines(
             csnake.Variable(
                 "input_tensor",
-                primitive=f"ap_int<{config_dict['INPUT_DATAWIDTH']}>",
+                primitive=f"TInput",
                 value=input_tensor,
             ).generate_initialization()
         )
         cwr.add_lines(
             csnake.Variable(
                 "weight_tensor",
-                primitive=f"ap_int<{config_dict['WEIGHT_DATAWIDTH']}>",
+                primitive=f"TWeight",
                 value=weight_tensor,
             ).generate_initialization()
         )
         cwr.add_lines(
             csnake.Variable(
                 "bias_tensor",
-                primitive=f"ap_int<{config_dict['BIAS_DATAWIDTH']}>",
+                primitive=f"TBias",
                 value=bias_tensor,
             ).generate_initialization()
         )
         cwr.add_lines(
             csnake.Variable(
                 "output_tensor",
-                primitive=f"ap_int<{config_dict['OUTPUT_DATAWIDTH']}>",
+                primitive=f"TOutput",
                 value=y,
             ).generate_initialization()
         )
@@ -369,7 +457,7 @@ class TestStreamingConv(BaseHLSTest):
             "PIPELINE_DEPTH": 3,
         }
         self.run(config_dict, hls_steps)
-    
+
     def test_pointwise_pertensor_chpar12_wpar4(self, hls_steps):
         np.random.seed(42)
         config_dict = {
@@ -399,6 +487,46 @@ class TestStreamingConv(BaseHLSTest):
             "X_SCALE": 2**-8,
             "W_SCALE": 2**-8,
             "Y_SCALE": 2**-6,
+            "X_ZP": 0,
+            "W_ZP": 0,
+            "Y_ZP": 0,
+            "PIPELINE_DEPTH": 5,
+        }
+        self.run(config_dict, hls_steps)
+
+    def test_pointwise_pertensor_uint(self, hls_steps):
+        np.random.seed(42)
+        config_dict = {
+            "ACC_DATAWIDTH": 25,
+            "INPUT_DATAWIDTH": 8,
+            "INPUT_IS_UNSIGNED": True,
+            "WEIGHT_DATAWIDTH": 8,
+            "WEIGHT_IS_UNSIGNED": False,
+            "BIAS_DATAWIDTH": 32,
+            "BIAS_IS_UNSIGNED": False,
+            "OUTPUT_DATAWIDTH": 8,
+            "OUTPUT_IS_UNSIGNED": True,
+            "OUT_HEIGHT": 16,
+            "OUT_WIDTH": 16,
+            "IN_CH": 16,
+            "OUT_CH": 32,
+            "FH": 3,
+            "FW": 3,
+            "STRIDE_H": 2,
+            "STRIDE_W": 2,
+            "PAD_T": 1,
+            "PAD_B": 1,
+            "PAD_L": 1,
+            "PAD_R": 1,
+            "DIL_H": 1,
+            "DIL_W": 1,
+            "IN_CH_PAR": 4,
+            "OUT_CH_PAR": 4,
+            "W_PAR": 1,
+            "GROUP": 1,
+            "X_SCALE": 2**-5,
+            "W_SCALE": 2**-8,
+            "Y_SCALE": 2**-5,
             "X_ZP": 0,
             "W_ZP": 0,
             "Y_ZP": 0,
