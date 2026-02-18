fa2dd9064ca077d85de42fa487ef7bb280e01fef
robertoBosio <roberto.bosio98@gmail.com>
2026-02-18 07:36:27 +0000

    fix: add test for rounding to nearest even correctness

--- tests/test_StreamingAveragePool.py
+++ tests/test_StreamingAveragePool.py
@@ -4,18 +4,18 @@ import csnake
 from onnx import TensorProto, helper
 from .base_hls_test import BaseHLSTest
 
-class TestStreamingGlobalAveragePool(BaseHLSTest):
+
+class TestStreamingAveragePool(BaseHLSTest):
 
     @property
     def operator_filename(self):
-        return "StreamingGlobalAveragePool"
-
+        return "StreamingAveragePool"
     @property
     def unit_filename(self):
-        return "StreamingGlobalAveragePool"
+        return "StreamingAveragePool"
 
     def generate_config_file(self, config_dict):
-        in_unsigned = bool(config_dict.get("INPUTA_IS_UNSIGNED", False))
+        in_unsigned = bool(config_dict.get("INPUT_IS_UNSIGNED", False))
         out_unsigned = bool(config_dict.get("OUTPUT_IS_UNSIGNED", False))
 
         in_bits = int(config_dict["INPUT_DATAWIDTH"])
@@ -25,18 +25,30 @@ class TestStreamingGlobalAveragePool(BaseHLSTest):
         onnx_out_type = self.get_tensorproto_dtype(out_bits, out_unsigned)
         np_in_type = self.get_numpy_dtype(in_bits, in_unsigned)
         np_out_type = self.get_numpy_dtype(out_bits, out_unsigned)
-        
+
         # random tensors
         in_info = np.iinfo(np_in_type)
         input_tensor = np.random.randint(
-            in_info.min,
-            in_info.max + 1,
-            size=(1, config_dict["OUT_CH"], config_dict["IN_HEIGHT"], config_dict["IN_WIDTH"]),
+            int(in_info.min),
+            int(in_info.max) + 1,
+            size=(
+                1,
+                config_dict["OUT_CH"],
+                config_dict["IN_HEIGHT"],
+                config_dict["IN_WIDTH"],
+            ),
             dtype=np_in_type,
         )
 
         X = helper.make_tensor_value_info(
-            "X", onnx_in_type, [1, config_dict["OUT_CH"], config_dict["IN_HEIGHT"], config_dict["IN_WIDTH"]]
+            "X",
+            onnx_in_type,
+            [
+                1,
+                config_dict["OUT_CH"],
+                config_dict["IN_HEIGHT"],
+                config_dict["IN_WIDTH"],
+            ],
         )
         Y = helper.make_tensor_value_info(
             "Y",
@@ -64,10 +76,20 @@ class TestStreamingGlobalAveragePool(BaseHLSTest):
             outputs=["X_dq"],
         )
 
-        global_avg_pool = helper.make_node(
-            "GlobalAveragePool",
+        avg_pool = helper.make_node(
+            "AveragePool",
             inputs=["X_dq"],
             outputs=["Y_dq"],
+            kernel_shape=[config_dict["FH"], config_dict["FW"]],
+            strides=[config_dict["STRIDE_H"], config_dict["STRIDE_W"]],
+            pads=[
+                config_dict["PAD_T"],
+                config_dict["PAD_L"],
+                config_dict["PAD_B"],
+                config_dict["PAD_R"],
+            ],
+            ceil_mode=config_dict["CEIL_MODE"],
+            count_include_pad=1,
         )
 
         qlinear = helper.make_node(
@@ -77,7 +99,7 @@ class TestStreamingGlobalAveragePool(BaseHLSTest):
         )
 
         graph = helper.make_graph(
-            [dqlinear, global_avg_pool, qlinear],
+            [dqlinear, avg_pool, qlinear],
             "qconv_test",
             [X],
             [Y],
@@ -88,10 +110,10 @@ class TestStreamingGlobalAveragePool(BaseHLSTest):
             model.SerializeToString(), providers=["CPUExecutionProvider"]
         )
         y = sess.run(None, {"X": input_tensor})[0]
-        
+
         shift = int(np.log2(config_dict["Y_SCALE"] / config_dict["X_SCALE"]))
-        tacc_bits = int(in_bits + int(np.ceil(np.log2(config_dict["IN_HEIGHT"] * config_dict["IN_WIDTH"]))))
-        tdiv_bits = int(np.ceil(np.log2((config_dict["IN_HEIGHT"] * config_dict["IN_WIDTH"]) + 1)))
+        tacc_bits = int(in_bits + int(np.ceil(np.log2(config_dict["FH"] * config_dict["FW"]))))
+        tdiv_bits = int(np.ceil(np.log2((config_dict["FH"] * config_dict["FW"]) + 1)))
         config_dict["ACC_DATAWIDTH"] = tacc_bits
         config_dict["DIV_DATAWIDTH"] = tdiv_bits
 
@@ -135,59 +157,95 @@ class TestStreamingGlobalAveragePool(BaseHLSTest):
         cwr.dedent()
         cwr.add_line("}")
         return cwr.code
-    
-    def test_7x7_po2(self, hls_steps):
+
+    def test_3x3_signed_po2(self, hls_steps):
         np.random.seed(42)
         config_dict = {
             "INPUT_DATAWIDTH": 8,
-            "INPUTA_IS_UNSIGNED": True,
+            "INPUT_IS_UNSIGNED": False,
             "OUTPUT_DATAWIDTH": 8,
-            "OUTPUT_IS_UNSIGNED": True,
-            "IN_HEIGHT": 7,
-            "IN_WIDTH": 7,
-            "OUT_CH": 100,
-            "OUT_CH_PAR": 5,
+            "OUTPUT_IS_UNSIGNED": False,
+            "IN_HEIGHT": 20,
+            "IN_WIDTH": 20,
+            "OUT_CH": 20,
+            "CH_PAR": 1,
+            "W_PAR": 1,
+            "FH": 3,
+            "FW": 3,
+            "STRIDE_H": 1,
+            "STRIDE_W": 1,
+            "DIL_H": 1,
+            "DIL_W": 1,
+            "PAD_T": 1,
+            "PAD_B": 1,
+            "PAD_L": 1,
+            "PAD_R": 1,
+            "CEIL_MODE": 0,
             "X_SCALE": 2**-5,
-            "Y_SCALE": 2**-5,
             "X_ZP": 0,
+            "Y_SCALE": 2**-5,
             "Y_ZP": 0,
             "PIPELINE_DEPTH": 5,
         }
         self.run(config_dict, hls_steps)
-    
-    def test_7x7_mixed_po2(self, hls_steps):
+
+    def test_2x2_nopad_signed_po2(self, hls_steps):
         np.random.seed(42)
         config_dict = {
             "INPUT_DATAWIDTH": 8,
-            "INPUTA_IS_UNSIGNED": True,
+            "INPUT_IS_UNSIGNED": False,
             "OUTPUT_DATAWIDTH": 8,
             "OUTPUT_IS_UNSIGNED": False,
-            "IN_HEIGHT": 7,
-            "IN_WIDTH": 7,
-            "OUT_CH": 1280,
-            "OUT_CH_PAR": 5,
+            "IN_HEIGHT": 20,
+            "IN_WIDTH": 20,
+            "OUT_CH": 20,
+            "CH_PAR": 1,
+            "W_PAR": 1,
+            "FH": 2,
+            "FW": 2,
+            "STRIDE_H": 1,
+            "STRIDE_W": 1,
+            "DIL_H": 1,
+            "DIL_W": 1,
+            "PAD_T": 0,
+            "PAD_B": 0,
+            "PAD_L": 0,
+            "PAD_R": 0,
+            "CEIL_MODE": 0,
             "X_SCALE": 2**-5,
-            "Y_SCALE": 2**-5,
             "X_ZP": 0,
+            "Y_SCALE": 2**-5,
             "Y_ZP": 0,
             "PIPELINE_DEPTH": 5,
         }
         self.run(config_dict, hls_steps)
-    
-    def test_2x2_signed_po2(self, hls_steps):
+
+    def test_7x7_nopad_mixed_po2(self, hls_steps):
         np.random.seed(42)
         config_dict = {
             "INPUT_DATAWIDTH": 8,
-            "INPUTA_IS_UNSIGNED": False,
+            "INPUT_IS_UNSIGNED": True,
             "OUTPUT_DATAWIDTH": 8,
             "OUTPUT_IS_UNSIGNED": False,
-            "IN_HEIGHT": 2,
-            "IN_WIDTH": 2,
-            "OUT_CH": 1280,
-            "OUT_CH_PAR": 5,
+            "IN_HEIGHT": 7,
+            "IN_WIDTH": 7,
+            "OUT_CH": 20,
+            "CH_PAR": 1,
+            "W_PAR": 1,
+            "FH": 7,
+            "FW": 7,
+            "STRIDE_H": 1,
+            "STRIDE_W": 1,
+            "DIL_H": 1,
+            "DIL_W": 1,
+            "PAD_T": 0,
+            "PAD_B": 0,
+            "PAD_L": 0,
+            "PAD_R": 0,
+            "CEIL_MODE": 0,
             "X_SCALE": 2**-5,
-            "Y_SCALE": 2**-5,
             "X_ZP": 0,
+            "Y_SCALE": 2**-5,
             "Y_ZP": 0,
             "PIPELINE_DEPTH": 5,
         }
