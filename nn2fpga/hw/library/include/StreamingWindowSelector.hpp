cd42244b11814725e601c565e153ebf21577d955
robertoBosio <roberto.bosio98@gmail.com>
2026-04-07 12:15:35 +0000

    chore: small improvements in resource usage when w_par > 1

--- nn2fpga/hw/library/include/StreamingWindowSelector.hpp
+++ nn2fpga/hw/library/include/StreamingWindowSelector.hpp
@@ -49,6 +49,7 @@ template <typename TWord, size_t IN_HEIGHT, size_t IN_WIDTH, size_t IN_CH,
           size_t CH_PAR>
 class StreamingWindowSelector {
   static constexpr size_t FW_EXPAND = FW + (W_PAR - 1) * STRIDE_W;
+
   static constexpr size_t TOP_BORDER = (PAD_T > POS_H) ? 0 : POS_H - PAD_T;
   static constexpr size_t LEFT_BORDER = (PAD_L > POS_W) ? 0 : POS_W - PAD_L;
   static constexpr size_t BOTTOM_BORDER =
@@ -56,24 +57,22 @@ class StreamingWindowSelector {
   static constexpr size_t RIGHT_BORDER =
       IN_WIDTH - ((FW_EXPAND - 1 - POS_W) - PAD_R);
 
-  // Stream in W_PAR considered in this pixel of the window. The amount
-  // W_PAR * PAD_L is only added to have an always positive value in the
-  // modulo operation, otherwise different compilers might handle it
-  // differently.
+  // Stream lane in W_PAR considered by this pixel.
   static constexpr size_t W_STREAM = (POS_W + PAD_L * (W_PAR - 1)) % W_PAR;
 
-  // Line considered by this pixel. STRIDE_H makes pixel skip some lines.
-  // The amount STRIDE_H * PAD_T is only added to have an always positive value
-  // in the modulo operation, otherwise different compilers might handle it
+  // Row modulo selected by this pixel.
   static constexpr size_t H_ROW_MOD =
       (POS_H + PAD_T * (STRIDE_H - 1)) % STRIDE_H;
 
-  // Column considered by this pixel. STRIDE_W, combined with W_PAR, makes pixel
-  // skip some columns. Again W_PAR * STRIDE_W is only added to have an always
-  // positive value in the modulo operation, otherwise different compilers might
-  // handle it differently.
+  // Word-position modulo selected by this pixel.
   static constexpr size_t W_COL_MOD =
       ((POS_W - PAD_L + (W_PAR * STRIDE_W)) / W_PAR) % STRIDE_W;
+
+  // Number of words seen by this stream lane across the input row.
+  // These correspond to columns:
+  //   W_STREAM, W_STREAM + W_PAR, W_STREAM + 2*W_PAR, ...
+  static constexpr size_t WORDS_THIS_STREAM =
+      (IN_WIDTH <= W_STREAM) ? 0 : ((IN_WIDTH - 1 - W_STREAM) / W_PAR + 1);
 
   static_assert(IN_CH % CH_PAR == 0, "IN_CH must be a multiple of CH_PAR");
   static_assert(FH > 0 && FW > 0, "FH and FW must be greater than 0");
@@ -81,8 +80,6 @@ class StreamingWindowSelector {
                 "STRIDE_H and STRIDE_W must be greater than 0");
   static_assert(DILATION_H > 0 && DILATION_W > 0,
                 "DILATION_H and DILATION_W must be greater than 0");
-  static_assert(PAD_T >= 0 && PAD_L >= 0 && PAD_B >= 0 && PAD_R >= 0,
-                "PAD_T, PAD_L, PAD_B and PAD_R must be non-negative");
   static_assert(POS_H < FH, "POS_H must be less than FH");
   static_assert(POS_W < FW_EXPAND,
                 "POS_W must be less than FW + (W_PAR-1)*STRIDE_W");
@@ -96,36 +93,42 @@ class StreamingWindowSelector {
                 "DILATION_H and DILATION_W must be 1");
 
 private:
-  static void pipeline_body(hls::stream<TWord> &i_data,
-                            hls::stream<TWord> &o_data,
-                            hls::stream<TWord> &o_shift_data, size_t i_h,
-                            size_t i_w, size_t i_w_stride) {
+  static constexpr size_t col_from_word(size_t i_w_word) {
+    return W_STREAM + i_w_word * W_PAR;
+  }
+
+  static bool match(size_t i_h, size_t i_w_word) {
 #pragma HLS inline
-    TWord in_word = i_data.read();
+    const size_t col = col_from_word(i_w_word);
 
     bool is_within_window = true;
     is_within_window &= (i_h >= TOP_BORDER && i_h < BOTTOM_BORDER);
-    is_within_window &= (i_w >= LEFT_BORDER && i_w < RIGHT_BORDER);
+    is_within_window &= (col >= LEFT_BORDER && col < RIGHT_BORDER);
     is_within_window &= ((i_h % STRIDE_H) == H_ROW_MOD);
-    is_within_window &= ((i_w_stride % STRIDE_W) == W_COL_MOD);
-    if (is_within_window) {
+    is_within_window &= ((i_w_word % STRIDE_W) == W_COL_MOD);
+    return is_within_window;
+  }
+
+  static void pipeline_body(hls::stream<TWord> &i_data,
+                            hls::stream<TWord> &o_data,
+                            hls::stream<TWord> &o_shift_data, size_t i_h,
+                            size_t i_w_word) {
+#pragma HLS inline
+    TWord in_word = i_data.read();
+
+    if (match(i_h, i_w_word)) {
       o_data.write(in_word);
     }
     o_shift_data.write(in_word);
   }
 
   static void pipeline_body(hls::stream<TWord> &i_data,
-                            hls::stream<TWord> &o_data, size_t i_h, size_t i_w,
-                            size_t i_w_stride) {
+                            hls::stream<TWord> &o_data, size_t i_h,
+                            size_t i_w_word) {
 #pragma HLS inline
     TWord in_word = i_data.read();
 
-    bool is_within_window = true;
-    is_within_window &= (i_h >= TOP_BORDER && i_h < BOTTOM_BORDER);
-    is_within_window &= (i_w >= LEFT_BORDER && i_w < RIGHT_BORDER);
-    is_within_window &= ((i_h % STRIDE_H) == H_ROW_MOD);
-    is_within_window &= ((i_w_stride % STRIDE_W) == W_COL_MOD);
-    if (is_within_window) {
+    if (match(i_h, i_w_word)) {
       o_data.write(in_word);
     }
   }
@@ -135,7 +138,10 @@ public:
 
   struct StepState {
     // Loop iteration indexes.
-    size_t i_h = 0, i_w = W_STREAM, i_w_stride = 0, i_ch = 0;
+    size_t i_h = 0;
+    size_t i_w_word = 0;
+    size_t i_ch = 0;
+
     PipelineDelayBuffer<TWord> delayed_output[2];
     ActorStatus actor_status{1, 1};
     bool initialized = false;
@@ -148,7 +154,7 @@ public:
       delayed_output[0] = PipelineDelayBuffer<TWord>(depth);
       delayed_output[1] = PipelineDelayBuffer<TWord>(depth);
       actor_status =
-          ActorStatus(depth, IN_HEIGHT * (IN_WIDTH / W_PAR) * (IN_CH / CH_PAR));
+          ActorStatus(depth, IN_HEIGHT * WORDS_THIS_STREAM * (IN_CH / CH_PAR));
       this->depth = depth;
       this->shift_stream_depth = shift_stream_depth;
       initialized = true;
@@ -156,6 +162,7 @@ public:
   };
 
   using Registry = std::unordered_map<const void *, StepState>;
+
   static Registry &registry() {
     static Registry r;
     return r;
@@ -169,15 +176,14 @@ public:
   template <size_t HLS_TAG>
   void run(hls::stream<TWord> &i_data, hls::stream<TWord> &o_data,
            hls::stream<TWord> &o_shift_data) {
-
     for (size_t i_h = 0; i_h < IN_HEIGHT; i_h++) {
-      for (size_t i_w = W_STREAM, i_w_stride = 0; i_w < IN_WIDTH;
-           i_w += W_PAR, i_w_stride++) {
+      for (size_t i_w_word = 0; i_w_word < WORDS_THIS_STREAM; i_w_word++) {
       WINDOWSELECTOR_RUN_LOOP:
         for (size_t i_ch = 0; i_ch < IN_CH / CH_PAR; i_ch++) {
 #pragma HLS pipeline II = 1
-          StreamingWindowSelector::pipeline_body(i_data, o_data, o_shift_data,
-                                                 i_h, i_w, i_w_stride);
+          StreamingWindowSelector::pipeline_body(i_data, o_data,
+                                                    o_shift_data, i_h,
+                                                    i_w_word);
         }
       }
     }
@@ -185,12 +191,10 @@ public:
 
   ActorStatus step(hls::stream<TWord> &i_data, hls::stream<TWord> &o_data,
                    hls::stream<TWord> &o_shift_data) {
-    // Find the state for this instance.
     auto it = registry().find(this);
     assert(it != registry().end() && "Instance not initialized");
     auto &st = it->second;
 
-    // Compute firing condition.
     bool firing_condition = true;
     if (i_data.size() < st.shift_stream_depth) {
       firing_condition = false;
@@ -199,47 +203,40 @@ public:
     if (firing_condition) {
       hls::stream<TWord> instant_o_data[1];
       hls::stream<TWord> instant_o_shift_data[1];
-      StreamingWindowSelector::pipeline_body(i_data, instant_o_data[0],
-                                             instant_o_shift_data[0], st.i_h,
-                                             st.i_w, st.i_w_stride);
+
+      StreamingWindowSelector::pipeline_body(
+          i_data, instant_o_data[0], instant_o_shift_data[0], st.i_h,
+          st.i_w_word);
 
       st.i_ch += CH_PAR;
       if (st.i_ch >= IN_CH) {
         st.i_ch = 0;
-        st.i_w += W_PAR;
-        st.i_w_stride++;
+        st.i_w_word++;
       }
-      if (st.i_w >= IN_WIDTH) {
-        st.i_w = W_STREAM;
+      if (st.i_w_word >= WORDS_THIS_STREAM) {
+        st.i_w_word = 0;
         st.i_h++;
-        st.i_w_stride = 0;
       }
       if (st.i_h >= IN_HEIGHT) {
         st.i_h = 0;
       }
 
-      // Insert the firing status for the current step.
       st.actor_status.fire();
 
-      // Add the output to the delayed output stream.
       if (!instant_o_data[0].empty()) {
         st.delayed_output[0].push(instant_o_data[0].read(), true);
       } else {
-        // If the output stream is empty, push a placeholder.
         st.delayed_output[0].push(TWord(), false);
       }
 
       if (!instant_o_shift_data[0].empty()) {
         st.delayed_output[1].push(instant_o_shift_data[0].read(), true);
       } else {
-        // If the output stream is empty, push a placeholder.
         st.delayed_output[1].push(TWord(), false);
       }
 
-      // Advance the state of the actor firings.
       st.actor_status.advance();
 
-      // Write the output data to the output stream.
       TWord out;
       if (st.delayed_output[0].pop(out)) {
         o_data.write(out);
@@ -255,25 +252,22 @@ public:
   template <size_t HLS_TAG>
   void run(hls::stream<TWord> &i_data, hls::stream<TWord> &o_data) {
     for (size_t i_h = 0; i_h < IN_HEIGHT; i_h++) {
-      for (size_t i_w = W_STREAM, i_w_stride = 0; i_w < IN_WIDTH;
-           i_w += W_PAR, i_w_stride++) {
+      for (size_t i_w_word = 0; i_w_word < WORDS_THIS_STREAM; i_w_word++) {
       WINDOWSELECTOR_RUN_LOOP:
         for (size_t i_ch = 0; i_ch < IN_CH / CH_PAR; i_ch++) {
 #pragma HLS pipeline II = 1
-          StreamingWindowSelector::pipeline_body(i_data, o_data, i_h, i_w,
-                                                 i_w_stride);
+          StreamingWindowSelector::pipeline_body(i_data, o_data, i_h,
+                                                    i_w_word);
         }
       }
     }
   }
 
   ActorStatus step(hls::stream<TWord> &i_data, hls::stream<TWord> &o_data) {
-    // Find the state for this instance.
     auto it = registry().find(this);
     assert(it != registry().end() && "Instance not initialized");
     auto &st = it->second;
 
-    // Compute firing condition.
     bool firing_condition = true;
     if (i_data.empty()) {
       firing_condition = false;
@@ -281,48 +275,41 @@ public:
 
     if (firing_condition) {
       hls::stream<TWord> instant_o_data[1];
-      StreamingWindowSelector::pipeline_body(i_data, instant_o_data[0], st.i_h,
-                                             st.i_w, st.i_w_stride);
+
+      StreamingWindowSelector::pipeline_body(i_data, instant_o_data[0],
+                                                st.i_h, st.i_w_word);
 
       st.i_ch += CH_PAR;
       if (st.i_ch >= IN_CH) {
         st.i_ch = 0;
-        st.i_w += W_PAR;
-        st.i_w_stride++;
+        st.i_w_word++;
       }
-      if (st.i_w >= IN_WIDTH) {
-        st.i_w = W_STREAM;
-        st.i_w_stride = 0;
+      if (st.i_w_word >= WORDS_THIS_STREAM) {
+        st.i_w_word = 0;
         st.i_h++;
       }
       if (st.i_h >= IN_HEIGHT) {
         st.i_h = 0;
       }
 
-      // Insert the firing status for the current step.
       st.actor_status.fire();
 
-      // Add the output to the delayed output stream.
       if (!instant_o_data[0].empty()) {
         st.delayed_output[0].push(instant_o_data[0].read(), true);
       } else {
-        // If the output stream is empty, push a placeholder.
         st.delayed_output[0].push(TWord(), false);
       }
     } else {
-      // If not firing, just advance the delayed output buffer.
       st.delayed_output[0].push(TWord(), false);
     }
 
-    // Advance the state of the actor firings.
     st.actor_status.advance();
 
-    // Write the output data to the output stream.
     TWord out;
     if (st.delayed_output[0].pop(out)) {
       o_data.write(out);
     }
-    
+
     return st.actor_status;
   }
 };
